15aef95eb5e9bc34eb87adbea90345fc32af7e5e
Josh Larson <jplhomer@gmail.com>
2015-11-08 21:24:43 -0600

    Use Tweet model in POD structure

--- app/models/tweet.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  avatar: DS.attr( 'string' ),
-  screen_name: DS.attr( 'string' ),
-  text: DS.attr( 'string' ),
-  date: DS.attr( 'string' )
-});
