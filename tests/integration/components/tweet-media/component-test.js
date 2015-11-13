eefbf6c9dc9db3b84e25b0b461fd4fea42281f7b
Josh Larson <jplhomer@gmail.com>
2015-11-12 20:37:58 -0600

    Add Tweet media component

---
+++ tests/integration/components/tweet-media/component-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('tweet-media', 'Integration | Component | tweet media', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{tweet-media}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#tweet-media}}
+      template block text
+    {{/tweet-media}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
