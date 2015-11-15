7614573b04c9da16ae96200a669603f13cfcfe36
Josh Larson <jplhomer@gmail.com>
2015-11-15 12:35:20 -0600

    Allow twitter images in security environment

--- config/environment.js
+++ config/environment.js
@@ -18,7 +18,7 @@ module.exports = function(environment) {
       'script-src': "'self'",
       'font-src': "'self' *",
       'connect-src': "'self' *",
-      'img-src': "'self'",
+      'img-src': "'self' *",
       'style-src': "'self' *",
       'media-src': "'self'"
     },
