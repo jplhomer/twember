78e009457958749adca0e9c14cb4fd70f5e37d7e
Josh Larson <jplhomer@gmail.com>
2015-11-15 13:12:04 -0600

    Try to fix failing test but failing

--- tests/integration/components/tweet-media-photo/component-test.js
+++ tests/integration/components/tweet-media-photo/component-test.js
@@ -8,19 +8,54 @@ moduleForComponent('tweet-media-photo', 'Integration | Component | tweet media p
 test('it renders', function(assert) {
   assert.expect(2);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{tweet-media-photo}}`);
 
   assert.equal(this.$().text().trim(), '');
 
+  this.set('media', {
+    "id": 663535229450084400,
+    "id_str": "663535229450084354",
+    "indices": [
+      96,
+      119
+    ],
+    "media_url": "http://pbs.twimg.com/media/CTVZxo9UYAI5Qs9.jpg",
+    "media_url_https": "https://pbs.twimg.com/media/CTVZxo9UYAI5Qs9.jpg",
+    "url": "https://t.co/emrdDiUIN3",
+    "display_url": "pic.twitter.com/emrdDiUIN3",
+    "expanded_url": "http://twitter.com/mashable/status/663535230649769985/photo/1",
+    "type": "photo",
+    "sizes": {
+      "thumb": {
+        "w": 150,
+        "h": 150,
+        "resize": "crop"
+      },
+      "small": {
+        "w": 340,
+        "h": 191,
+        "resize": "fit"
+      },
+      "medium": {
+        "w": 600,
+        "h": 337,
+        "resize": "fit"
+      },
+      "large": {
+        "w": 949,
+        "h": 534,
+        "resize": "fit"
+      }
+    }
+  });
+
   // Template block usage:
   this.render(hbs`
-    {{#tweet-media-photo}}
-      template block text
-    {{/tweet-media-photo}}
+    {{#tweet-media-photo media=media}}{{/tweet-media-photo}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().children().find('.block__media').attr('class'), 'block__media block__media--photo');
+
+  // ????
+  assert.equal(this.$().find('img')[0], '<img alt="Photo" src="https://pbs.twimg.com/media/CTVZxo9UYAI5Qs9.jpg"></img>');
 });
