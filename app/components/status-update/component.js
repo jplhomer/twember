988cda54d0ada31bb140219a21d59a5afcbf5fd2
Josh Larson <jplhomer@gmail.com>
2015-11-16 20:50:05 -0600

    Add more rows when it is expanded

--- app/components/status-update/component.js
+++ app/components/status-update/component.js
@@ -3,15 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['block block--status-update'],
 
+  numRows: 1,
+
   actions: {
     contractForm() {
       if ( ! this.$('textarea').val().length ) {
         this.set('isExpanded', false);
+        this.set('numRows', 1);
       }
     },
 
     expandForm() {
       this.set('isExpanded', true);
+      this.set('numRows', 3);
     }
   }
 });
