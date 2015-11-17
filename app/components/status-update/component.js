c05878f99811e09d1dbecea7a06d52e03012b32d
Josh Larson <jplhomer@gmail.com>
2015-11-16 20:38:16 -0600

    Add and style component for status update

--- app/components/status-update/component.js
+++ app/components/status-update/component.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['block block--status-update']
+  classNames: ['block block--status-update'],
+
+  actions: {
+    contractForm() {
+      if ( ! this.$('textarea').val().length ) {
+        this.set('isExpanded', false);
+      }
+    },
+
+    expandForm() {
+      this.set('isExpanded', true);
+    }
+  }
 });
