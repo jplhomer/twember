ad80ec8af8ffd3db2d3024c1dd52346a6ea90283
Josh Larson <jplhomer@gmail.com>
2015-11-27 12:21:13 -0600

    Sort tweets properly

--- app/components/tweet-list/component.js
+++ app/components/tweet-list/component.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['tweets'],
 
+  sortedTweets: Ember.computed.sort('tweets', 'tweetSorting'),
+
+  tweetSorting: ['created_at:desc'],
+
   tagName: 'ul'
 });
