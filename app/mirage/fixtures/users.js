fa5fdfb7781608fd4b23b583ff341e61e9f86add
Josh Larson <jplhomer@gmail.com>
2015-11-28 12:46:52 -0600

    Start to wire up profile page

--- app/mirage/fixtures/users.js
+++ app/mirage/fixtures/users.js
@@ -1,12 +1,12 @@
 export default [
   {
-    createdAt: new Date(),
-    isVerified: false,
+    created_at: new Date(),
+    is_verified: false,
     name: 'Josh Larson',
-    profileImageUrlHttps: 'https://pbs.twimg.com/profile_images/646425256/JoshSpain_bigger.jpg',
-    profileBannerUrl: 'https://pbs.twimg.com/profile_banners/17814884/1431358881/600x200',
-    screenName: 'jplhomer',
-    followersCount: 10,
-    friendsCount: 10
+    profile_image_url_https: 'https://pbs.twimg.com/profile_images/646425256/JoshSpain_bigger.jpg',
+    profile_banner_url: 'https://pbs.twimg.com/profile_banners/17814884/1431358881/600x200',
+    screen_name: 'jplhomer',
+    followers_count: 10,
+    friends_count: 10
   }
 ];
--- app/user/model.js
+++ app/user/model.js
@@ -1,13 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  createdAt: DS.attr( 'date' ),
-  isVerified: DS.attr( 'boolean' ),
+  created_at: DS.attr( 'date' ),
+  is_verified: DS.attr( 'boolean' ),
   name: DS.attr( 'string' ),
-  profileImageUrlHttps: DS.attr( 'string' ),
-  profileBannerUrl: DS.attr( 'string' ),
-  screenName: DS.attr( 'string' ),
-  followersCount: DS.attr( 'number' ),
-  friendsCount: DS.attr( 'number' ),
-  statusesCount: DS.attr( 'number' )
+  profile_image_url_https: DS.attr( 'string' ),
+  profile_banner_url: DS.attr( 'string' ),
+  screen_name: DS.attr( 'string' ),
+  followers_count: DS.attr( 'number' ),
+  friends_count: DS.attr( 'number' ),
+  statuses_count: DS.attr( 'number' )
 });
